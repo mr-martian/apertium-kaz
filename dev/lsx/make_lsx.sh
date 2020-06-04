ff5f25a3878de3fc571accfda5e429607ca6beb6
Daniel Swanson <popcorn.tomato.dude@gmail.com>
2020-06-04 16:29:06 -0400

    forgot a step

--- dev/lsx/make_lsx.sh
+++ dev/lsx/make_lsx.sh
@@ -3,4 +3,6 @@
 tail -n +1952 ../../apertium-kaz.kaz.lexc | grep "% " | sed 's/%//g' | sed -E 's/:.*//' > multi.txt
 ./extract.py
 ./morph.py
+grep "*" multi_split.txt > multi_star.txt
+grep -v "*" multi_split.txt > multi_no_star.txt
 ./to_lsx.py
